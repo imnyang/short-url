e7ecb5fd2ac4c82895f35c497e21876c815feaad
HYONSU <admin@hyonsu.com>
2024-01-13 03:45:34 +0900

    디버그 기능 추가

--- web/routes/test.js
+++ web/routes/test.js
@@ -2,17 +2,27 @@ const express = require('express');
 
 const app = express.Router();
 
-app.get('/device', (req, res) => {
+const main = require('../../main');
+
+app.get('/debug/device', (req, res) => {
     res.type('text');
     res.send(JSON.stringify(req.useragent, null, 2));
 });
 
-app.get('/locale', (req, res) => {
+app.get('/debug/locale', (req, res) => {
     res.send(req.get('Accept-Language')?.substring(0, 2) || 'en');
 });
 
-app.get('/error/:code', (req, res) => {
+app.get('/debug/error/:code', (req, res) => {
     res.status(parseInt(req.params.code)).end();
+});
+
+app.get('/debug/user', async (req, res) => {
+    if(!req.isAuthenticated()) return res.redirect(`/login?redirect_url=${encodeURIComponent(req.originalUrl)}`);
+
+    if(!main.getOwnerID().includes(req.user.id)) return res.status(403).end();
+
+    res.send(JSON.stringify(req.user, null, 2));
 });
 
 module.exports = app;
