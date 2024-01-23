ffc2885795b0ac2b11700a65859137cb543f9b1e
HYONSU <admin@hyonsu.com>
2024-01-24 07:12:39 +0900

    parseUrl에서 링크가 루트인 경우 https:// 추가하지 않기

--- utils.js
+++ utils.js
@@ -396,7 +396,7 @@ module.exports.getAllUrlInString = str => {
 
 module.exports.parseUrl = str => {
     try {
-        if(str.includes('/') && !str.startsWith('http')) str = `https://${str}`;
+        if(str.includes('/') && !str.startsWith('http') && str.length > 1) str = `https://${str}`;
 
         const url = new URL(str);
         return {
